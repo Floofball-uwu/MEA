d406516e2615712b37380d0ab4cf47c649416d5b
Farmer Thanos <wiktorlk@hotmail.com>
2021-04-04 12:07:13 +0200

    Testing megacatswoop's code

--- src/farmerthanos/mea/Calls.java
+++ src/farmerthanos/mea/Calls.java
@@ -2,7 +2,23 @@ package farmerthanos.mea;
 
 import mindustry.gen.Building;
 import mindustry.gen.Building.*;
+import mindustry.gen.Groups;
+
+import static mindustry.Vars.*;
+
 
 public class Calls {
-
+    public void ohno(){
+        //I thank megacatswoop#7269 for this
+        for (Building build : Groups.build) {
+            Groups.bullet.intersect(build.x - (build.block().size / 2) - 10 * 8,
+                    build.y - (build.block().size / 2) - 10 * 8,
+                    10 * 8, 10 * 8, bullet -> {
+                if (bullet.team != build.team) {
+                    Announcements.BaseUnderAttack.play();
+                }
+            });
+            break;
+        }
+    }
 }
